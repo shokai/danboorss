410741760403ecbdacc685a475a71db00a679a0c
Sho Hashimoto <hashimoto@shokai.org>
2012-06-12 16:12:21 +0900

    separate controller/feed.rb

---
+++ controllers/feed.rb
@@ -0,0 +1,22 @@
+before '/*.rss' do
+  content_type 'application/xml'
+end
+
+get '/:tags.rss' do
+  @tags = params[:tags].split(/\s/)
+  @title = "danboorss #{@tags}"
+  imgs = TmpCache.get(@tags.join('+')) || TmpCache.set(@tags.join('+'), Danbooru.search(@tags), 3600*4)
+  rss = RSS::Maker.make('2.0') do |rss|
+    rss.channel.about = "#{app_root}#{ENV['PATH_INFO']}"
+    rss.channel.title = @title
+    rss.channel.link = Danbooru.search_url @tags
+    rss.channel.description = @title
+    imgs.each do |img|
+      i = rss.items.new_item
+      i.title = img.permalink
+      i.link = img.permalink
+      i.description = "<p><img src=\"#{img.url}\"></p>"
+    end
+  end
+  rss.to_s
+end
