f1a92ea759f806cc176dd53967bb5a7e1b8f4cd6
Sho Hashimoto <hashimoto@shokai.org>
2012-06-12 21:22:11 +0900

    fix feed title

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -4,14 +4,13 @@ end
 
 get '/tag/:tags.rss' do
   @tags = params[:tags].split(/\s/)
-  @title = "danboorss #{@tags}"
   search = TmpCache.get(@tags.join('+')) || TmpCache.set(@tags.join('+'), Danbooru.search(@tags), 3600*4)
-  make_rss("danboorss #{@tags}", search[:uri], search[:images]).to_s
+  make_rss("danboorss - #{@tags}", search[:uri], search[:images]).to_s
 end
 
 get '/popular/:type.rss' do
   @type = params[:type].to_sym
   throw :halt, [404, 'not found'] unless [:day, :month, :week].include? @type
   popular = TmpCache.get(@type) || TmpCache.set(@type, Danbooru.popular(@type), 3600*4)
-  make_rss("danboorss popular by #{@type}", popular[:uri], popular[:images]).to_s
+  make_rss("danboorss - popular by #{@type}", popular[:uri], popular[:images]).to_s
 end
